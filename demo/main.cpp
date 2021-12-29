f66adfab9cd6c1865416ec482e2378b40c6165b4
Maryan Yaroma <yaroma.maryan@gmail.com>
2021-12-30 01:48:36 +0300

    Added draw triangle

--- demo/main.cpp
+++ demo/main.cpp
@@ -2,6 +2,11 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include "shader.h"
+#include "resource_manager.h"
 
 namespace
 {
@@ -11,13 +16,12 @@ namespace
 
 void ResizeCallback(GLFWwindow* window, int width, int height);
 void ProcessInput(GLFWwindow* window);
-
+std::string LoadShaderSource(const std::string& file_name);
 
 int main()
 {
     std::cout << "Start demo!" << std::endl;
 
-    // init glfw
     glfwInit();
     glfwInitHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwInitHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -44,6 +48,32 @@ int main()
         return EXIT_FAILURE;
     }
 
+    auto shader = ResourceManager::LoadShader("shaders/default.vert", "shaders/default.frag", "default");
+
+    float vertices[] = 
+    {
+        -0.5f, -0.5f, 0.0f, // left
+         0.5f, -0.5f, 0.0f, // right
+         0.0f,  0.5f, 0.0f  // top
+    };
+
+    unsigned int VBO;
+    unsigned int VAO;
+
+    glGenBuffers(1, &VBO);
+    glGenVertexArrays(1, &VAO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
     while (! glfwWindowShouldClose(window))
     {
         ProcessInput(window);
@@ -51,9 +81,16 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        shader.Use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
     
     glfwTerminate();
     return EXIT_SUCCESS;
