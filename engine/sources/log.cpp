84ad7a8b63f753581b1b10611fbf52569acd62ae
Maryan Yaroma <yaroma.maryan@gmail.com>
2022-01-17 00:15:43 +0300

    Added custom log

---
+++ engine/sources/log.cpp
@@ -0,0 +1,32 @@
+#include "log.h"
+
+#include <cstdarg>
+#include <cstdlib>
+#include <iostream>
+
+namespace sge
+{
+    void Log::info(const char *format, ...)
+    {
+        va_list args;
+        va_start(args, format);
+        vfprintf(stdout, format, args);
+        va_end(args);
+    }
+
+    void Log::warning(const char *format, ...)
+    {
+        va_list args;
+        va_start(args, format);
+        vfprintf(stderr, format, args);
+        va_end(args);
+    }
+
+    void Log::error(const char *format, ...)
+    {
+        va_list args;
+        va_start(args, format);
+        vfprintf(stderr, format, args);
+        va_end(args);
+    }
+}
