0a3a75a9fd28349c3dbcc11a49e91bf06a1ceb5e
Maryan Yaroma <yaroma.maryan@gmail.com>
2024-12-22 11:13:36 +0100

    Added basic CMake setup and a minimal stub for the engine library.

---
+++ engine/private/application.cpp
@@ -0,0 +1,6 @@
+#include "application.h"
+
+namespace SGE
+{
+    Application::Application(){}
+}
---
+++ engine/public/application.h
@@ -0,0 +1,13 @@
+#ifndef _SGE_APPLICATION_H_
+#define _SGE_APPLICATION_H_
+
+namespace SGE
+{
+    class Application
+    {
+    public:
+        Application();
+    };
+}
+
+#endif // !_SGE_APPLICATION_H_
