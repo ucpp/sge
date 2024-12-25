01875698602f9338c96988d8ea7bcf2fe4d4a9f3
Maryan Yaroma <yaroma.maryan@gmail.com>
2024-12-25 18:12:07 +0100

    Added a Fence class to manage synchronization.

---
+++ engine/private/sge_fence.cpp
@@ -0,0 +1,52 @@
+#include "sge_fence.h"
+
+#include "sge_helpers.h"
+
+namespace SGE
+{
+    Fence::~Fence()
+    {
+        if (m_fenceEvent)
+        {
+            CloseHandle(m_fenceEvent);
+            m_fenceEvent = nullptr;
+        }
+    }
+
+    void Fence::Initialize(Device* device, UINT64 initialValue)
+    {
+        HRESULT hr = device->GetDevice()->CreateFence(initialValue, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence));
+        Verify(hr, "Failed to create D3D12 fence.");
+
+        m_fenceValue = initialValue;
+        m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+        Verify(m_fenceEvent, "Failed to create event handle.");
+    }
+
+    void Fence::Signal(ID3D12CommandQueue* commandQueue)
+    {
+        m_fenceValue++;
+        HRESULT hr = commandQueue->Signal(m_fence.Get(), m_fenceValue);
+        Verify(hr, "Failed to signal command queue.");
+    }
+
+    void Fence::Wait(UINT64 waitValue)
+    {
+        if (waitValue == 0)
+        {
+            waitValue = m_fenceValue;
+        }
+
+        if (m_fence->GetCompletedValue() < waitValue)
+        {
+            HRESULT hr = m_fence->SetEventOnCompletion(waitValue, m_fenceEvent);
+            Verify(hr, "Failed to set event on fence completion.");
+            WaitForSingleObject(m_fenceEvent, INFINITE);
+        }
+    }
+
+    UINT64 Fence::GetCompletedValue() const
+    {
+        return m_fence->GetCompletedValue();
+    }
+}
---
+++ engine/public/sge_fence.h
@@ -0,0 +1,28 @@
+#ifndef _SGE_FENCE_H_
+#define _SGE_FENCE_H_
+
+#include "sge_device.h"
+
+namespace SGE
+{
+    class Fence final
+    {
+    public:
+        Fence() = default;
+        ~Fence();
+
+        void Initialize(Device* device, UINT64 initialValue = 0);
+        void Signal(ID3D12CommandQueue* commandQueue);
+        void Wait(UINT64 waitValue = 0);
+
+        UINT64 GetCompletedValue() const;
+        UINT64 GetCurrentFenceValue() const { return m_fenceValue; }
+
+    private:
+        ComPtr<ID3D12Fence> m_fence;
+        UINT64 m_fenceValue = 0;
+        HANDLE m_fenceEvent = nullptr;
+    };
+}
+
+#endif // !_SGE_FENCE_H_
