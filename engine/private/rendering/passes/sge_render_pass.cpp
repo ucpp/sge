881bab576a1f30d5525bd90925257aa8e30d4e4a
Maryan Yaroma <yaroma.maryan@gmail.com>
2025-02-22 18:25:22 +0100

    Fixed incorrect error message text.

--- engine/private/rendering/passes/sge_render_pass.cpp
+++ engine/private/rendering/passes/sge_render_pass.cpp
@@ -132,7 +132,7 @@ namespace SGE
 
     void RenderPass::DrawModels(Scene* scene)
     {
-        Verify(m_context, "ForwardRenderPass::OnDraw: Render context is null.");
+        Verify(m_context, "RenderPass::OnDraw: Render context is null.");
         ID3D12GraphicsCommandList* commandList = m_context->GetCommandList().Get();
         commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
